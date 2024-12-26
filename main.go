cfbd8b9942868f8d977bdc96b1a61c107904ddb2
ramuneri <ramune.riau@gmail.com>
2024-12-26 11:48:48 +0200

    Lesson 12

--- main.go
+++ main.go
@@ -5,10 +5,32 @@ import (
 )
 
 func main() {
-
-    sayHello("Ramune")
-
-    for _, v := range points {
-        fmt.Println(v)
+    menu := map[string]float64 {
+        "soup": 1.99,
+        "pie": 3.99,
+        "salad": 4.99,
+        "pizza": 7.99,
     }
+
+    fmt.Println(menu)
+    fmt.Println(menu["pie"])
+
+    for k, v := range menu {
+        fmt.Println(k, "-", v)
+    }
+
+
+    phoneBook := map[int]string {
+        1234: "Ramune",
+        5678: "Ruta",
+        9101: "Marijus",
+    }
+
+    fmt.Println(phoneBook)
+    fmt.Println(phoneBook[5678])
+
+    phoneBook[1234] = "Ramunele"
+    fmt.Println(phoneBook)
+
+
 }
