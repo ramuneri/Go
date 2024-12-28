907851a75b1df91da4e8fb3274498c6061ce7a64
ramuneri <ramune.riau@gmail.com>
2024-12-28 16:58:40 +0200

    Lesson 14

--- main.go
+++ main.go
@@ -4,33 +4,20 @@ import (
     "fmt"
 )
 
+func updateName(x *string) {
+    *x = "Marijus"
+}
+
 func main() {
-    menu := map[string]float64 {
-        "soup": 1.99,
-        "pie": 3.99,
-        "salad": 4.99,
-        "pizza": 7.99,
-    }
+    name := "Ramune"
 
-    fmt.Println(menu)
-    fmt.Println(menu["pie"])
+    m := &name
+    fmt.Println("memory address is: ", m)
+    fmt.Println("value there is: ", *m)
 
-    for k, v := range menu {
-        fmt.Println(k, "-", v)
-    }
-
-
-    phoneBook := map[int]string {
-        1234: "Ramune",
-        5678: "Ruta",
-        9101: "Marijus",
-    }
-
-    fmt.Println(phoneBook)
-    fmt.Println(phoneBook[5678])
-
-    phoneBook[1234] = "Ramunele"
-    fmt.Println(phoneBook)
+    fmt.Println(name)
+    updateName(m)
+    fmt.Println(name)
 
 
 }
